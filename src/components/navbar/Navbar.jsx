c28319358278c48f721ebe03006b7bf311f55655
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-04-08 22:48:23 +0000

    completed the homepage

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,25 @@
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="left">Shop</div>
       <div className="right">
-        <span>Home</span>
-        <span>About</span>
-        <span>Contact</span>
-        <span>Login</span>
+        <span>
+          <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+            Home
+          </Link>
+        </span>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          About
+        </Link>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          Contact
+        </Link>
+        <Link to="/login" style={{ color: "inherit", textDecoration: "none" }}>
+          Login
+        </Link>
       </div>
     </div>
   );
