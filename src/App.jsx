6795c0f75fbd01a5d8c21472a3cdbab02d9adcf7
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-05-26 14:52:56 +0000

    did a little work on the routing

--- src/App.jsx
+++ src/App.jsx
@@ -13,25 +13,47 @@ import AudiosPage from "./pages/audios-page/AudiosPage";
 import PdfPage from "./pages/pdfPage/PdfPage";
 import SinglePdf from "./pages/single-pdf/SinglePdf";
 import SendEmail from "./components/SendEmail";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext/AuthContext";
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
     <div>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/images" element={<ImagesPage />} />
-        <Route path="/videos" element={<VideosPage />} />
-        <Route path="/audios" element={<AudiosPage />} />
-        <Route path="/pdf" element={<PdfPage />} />
-        <Route path="/images/:id" element={<SingleImage />} />
-        <Route path="/videos/:id" element={<SingleVideo />} />
-        <Route path="/pdf/:id" element={<SinglePdf />} />
-        <Route path="api/auth/:id/verify/:token" element={<Email />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/password-reset/:id/:token" element={<PasswordReset />} />
-        <Route path="/sendEmail/:id" element={<SendEmail />} />
+
+        <Route path="/images" element={user ? <ImagesPage /> : <Login />} />
+        <Route path="/videos" element={user ? <VideosPage /> : <Login />} />
+        <Route path="/audios" element={user ? <AudiosPage /> : <Login />} />
+        <Route path="/pdf" element={user ? <PdfPage /> : <Login />} />
+        <Route
+          path="/images/:id"
+          element={user ? <SingleImage /> : <Login />}
+        />
+        <Route
+          path="/videos/:id"
+          element={user ? <SingleVideo /> : <Login />}
+        />
+        <Route path="/pdf/:id" element={user ? <SinglePdf /> : <Login />} />
+        <Route
+          path="api/auth/:id/verify/:token"
+          element={user ? <Email /> : <Login />}
+        />
+        <Route
+          path="/forgot-password"
+          element={user ? <ForgotPassword /> : <Login />}
+        />
+        <Route
+          path="/password-reset/:id/:token"
+          element={user ? <PasswordReset /> : <Login />}
+        />
+        <Route
+          path="/sendEmail/:id"
+          element={user ? <SendEmail /> : <Login />}
+        />
       </Routes>
     </div>
   );
