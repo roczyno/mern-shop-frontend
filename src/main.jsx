0247d07d1674935315987634351ec0d0207405f5
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-05-30 17:02:26 +0000

    d

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </AuthContextProvider>
   </React.StrictMode>
 );
